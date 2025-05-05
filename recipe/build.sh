f8d993b607489c4391db8dbb599cfe4a2f8f28f2
Serhii Kupriienko <skupriienko@anaconda.com>
2025-05-05 16:42:56 +0300

    2025.1

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,22 +1,17 @@
-#!/bin/bash
-
-./utils/git-sync-deps
-
+set -ex
 mkdir build
 cd build
 
-cmake \
-    -DCMAKE_INSTALL_PREFIX=$PREFIX \
-    -DCMAKE_PREFIX_PATH=$PREFIX \
-    ..
+if [[ "${target_platform}" == osx-64 ]]; then
+  CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+fi
+
+cmake ${CMAKE_ARGS} \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DSPIRV_TOOLS_LIBRARY_TYPE=SHARED \
+  -DSPIRV_TOOLS_BUILD_STATIC=OFF \
+  -DSPIRV-Headers_SOURCE_DIR="${PREFIX}" \
+  ..
 
 make -j${CPU_COUNT}
 make install
-
-
-cp tools/spirv-as $PREFIX/bin/spirv-as
-cp tools/spirv-dis $PREFIX/bin/spirv-dis
-cp tools/spirv-val $PREFIX/bin/spirv-val
-cp tools/spirv-opt $PREFIX/bin/spirv-opt
-cp tools/spirv-cfg $PREFIX/bin/spirv-cfg
-cp tools/spirv-link $PREFIX/bin/spirv-link
